a063872dd7b5d7eab10f99bf3367cd92d4084063
gardenk <jwnnekim@gmail.com>
2024-06-18 12:13:36 +0900

    FEAT: initial skeleton

--- SHELL/TestShellApplication.py
+++ SHELL/TestShellApplication.py
@@ -1,2 +1,29 @@
+import sys
+
+
 class TestShellApplication:
-    pass
+    def __init__(self):
+        pass
+
+    def write(self, address: int, data: str):
+        pass
+
+    def read(self, address: int):
+        pass
+
+    def fullwrite(self):
+        pass
+
+    def fullread(self):
+        pass
+
+
+if __name__ == '__main__':
+    while True:
+        _input = input()
+        print(_input)   # TODO ; will be deleted
+
+        if _input == 'exit':
+            sys.exit(0)
+
+        break  # TODO ; will be deleted
