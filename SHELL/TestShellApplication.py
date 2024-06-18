1bf2b2626dc0700a27f68cb89e050a0968559837
whlee <nextlwh@daum.net>
2024-06-18 15:02:19 +0900

    FEAT: write command validity check with test code

--- SHELL/TestShellApplication.py
+++ SHELL/TestShellApplication.py
@@ -35,12 +35,38 @@ class TestShellApplication:
     def help(self):
         pass
 
-    def is_valid_command(self, input_command):
-        pass
+    def is_valid_address(self, address):
+        for num in address:
+            if not ord('0') <= ord(num) <= ord('9'):
+                return False
+        if int(address) < 0 or 99 < int(address):
+            return False
+        return True
+
+    def is_valid_data_format(self, input_data):
+        if len(input_data) != 10:
+            return False
+        if input_data[0] != '0' or input_data[1] != 'x':
+            return False
+        for num in input_data[2:]:
+            if not (ord('0') <= ord(num) <= ord('9') or ord('A') <= ord(num) <= ord('F')):
+                return False
+        return True
+
+    def is_valid_command(self, input_commands):
+        if len(input_commands) > 3:
+            return False
+        if input_commands[0] == 'write':
+            if len(input_commands) != 3:
+                return False
+            if not self.is_valid_address(input_commands[1]):
+                return False
+            if not self.is_valid_data_format(input_commands[2]):
+                return False
+            return True
 
 
 def main():
-
     shell = TestShellApplication()
     while True:
         shell.run()
