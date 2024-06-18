c88c56f474a200306040c457cb92b85f1bc94973
shpark <sh0913.park@samsung.com>
2024-06-18 13:58:33 +0900

    test: add test_init_ssddrivercomma

--- SSD/ssd.py
+++ SSD/ssd.py
@@ -1,14 +1,33 @@
 from abc import ABC, abstractmethod
+import os
 
 
 class SSDDriver(ABC):
     def __init__(self, nand_path, result_path):
+        if not os.path.isfile(nand_path):
+            with open(nand_path, 'w') as file:
+                pass
         self.nand_path = nand_path
         self.result_path = result_path
+
     @abstractmethod
-    def read(self):
+    def read(self, addr):
         pass
 
     @abstractmethod
-    def write(self, value):
+    def write(self, addr, value):
         pass
+
+
+class SSDDriverComma(SSDDriver):
+    def __init__(self, nand_path, result_path):
+        super().__init__(nand_path, result_path)
+
+    def read(self):
+        buffer = ''
+        with open(self.nand_path, 'r') as nand:
+            buffer = nand.readlines()
+
+    def write(self):
+        pass
+
---
+++ SSD/test_ssd.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from ssd import SSDDriverComma
+import os
+
+
+class TestSSDDriver(TestCase):
+    def test_init_SSDDriverComma(self):
+        nand_path = 'nand_temp.txt'
+        result_path = 'result_temp.txt'
+        ssd = SSDDriverComma(nand_path, result_path)
+        self.assertTrue(os.path.isfile(nand_path))
+        self.clear_files(nand_path, result_path)
+
+    def clear_files(self, nand_path, result_path):
+        if os.path.exists(nand_path):
+            os.remove(nand_path)
+        if os.path.exists(result_path):
+            os.remove(result_path)
