3042b57a06fe2e57855ac53af1977e914675b431
shpark <sh0913.park@samsung.com>
2024-06-18 14:05:09 +0900

    refactor: add clear_files method

--- SSD/test_ssd.py
+++ SSD/test_ssd.py
@@ -11,6 +11,14 @@ class TestSSDDriver(TestCase):
         self.assertTrue(os.path.isfile(nand_path))
         self.clear_files(nand_path, result_path)
 
+    def test_write_SSDDriverComma(self):
+        nand_path = 'nand_temp.txt'
+        result_path = 'result_temp.txt'
+        ssd = SSDDriverComma(nand_path, result_path)
+        ssd.write()
+        self.assertTrue(os.path.isfile(nand_path))
+        self.clear_files(nand_path, result_path)
+
     def clear_files(self, nand_path, result_path):
         if os.path.exists(nand_path):
             os.remove(nand_path)
