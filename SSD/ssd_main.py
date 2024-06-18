3c494c91b3690ac8be4420460fdd40e84ad58fd0
Eunji.Heo <eunja.heo@samsung.com>
2024-06-18 14:18:29 +0900

    TEST: add test case for ssd arg parse interface

---
+++ SSD/ssd_main.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+
+
+class SSDApplication:
+    def main(self, args: list) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    app = SSDApplication()
+    print(app.main(sys.argv[1:]))
--- SSD/test_ssd.py
+++ SSD/test_ssd.py
@@ -1,4 +1,6 @@
 from unittest import TestCase
+
+from ssd_main import SSDApplication
 from ssd import SSDDriverComma
 import os
 
@@ -24,3 +26,35 @@ class TestSSDDriver(TestCase):
             os.remove(nand_path)
         if os.path.exists(result_path):
             os.remove(result_path)
+
+
+class TestSSDMain(TestCase):
+    def test_main_invalid_input_read(self):
+        app = SSDApplication()
+        ret = app.main(["R", "-1"])
+        self.assertEqual(ret, False)
+        ret = app.main(["W", "100"])
+        self.assertEqual(ret, False)
+
+    def test_main_read(self):
+        app = SSDApplication()
+        ret = app.main(["R", "-1"])
+        self.assertEqual(ret, True)
+
+    def test_main_invalid_input_write(self):
+        app = SSDApplication()
+        ret = app.main(["W", "-1", "0x00000000"])
+        self.assertEqual(ret, False)
+        ret = app.main(["W", "100", "0x00000000"])
+        self.assertEqual(ret, False)
+        ret = app.main(["W", "0", "1"])
+        self.assertEqual(ret, False)
+        ret = app.main(["W", "0", "0xFF"])
+        self.assertEqual(ret, False)
+        ret = app.main(["W", "0", "0x123456789"])
+        self.assertEqual(ret, False)
+
+    def test_main_write(self):
+        app = SSDApplication()
+        ret = app.main(["W", "0", "0x12345678"])
+        self.assertEqual(ret, True)
