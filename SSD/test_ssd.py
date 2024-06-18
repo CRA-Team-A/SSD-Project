69b2d1768398383f60a6b6bd29ecaf3d12dd866e
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-18 15:33:22 +0900

    [REFACTOR] SSDDriverEnter Read 기능 리팩토링

--- SSD/test_ssd.py
+++ SSD/test_ssd.py
@@ -3,21 +3,32 @@ from unittest import TestCase
 
 from SSD.ssd import SSDDriverEnter
 
+TEST_RESULT_FILE_PATH = 'result_tmp.txt'
+
+TEST_NAND_FILE_PATH = 'nand_tmp.txt'
+
+INITIAL_VALUE = "0x00000000"
+
 
 class TestSSDDriverEnter(TestCase):
-    def test_read_file_exists(self):
+    def setUp(self):
         initial_data = ""
         for i in range(20):
             initial_data += "0" + "\n"
-        nand_path = os.path.dirname(os.getcwd()) + '\\nand_tmp.txt'
-        result_path = os.path.dirname(os.getcwd()) + '\\result_tmp.txt'
-        with open(nand_path, 'w') as nand_file:
+        self.nand_path = os.path.dirname(os.getcwd()) + '\\' + TEST_NAND_FILE_PATH
+        self.result_path = os.path.dirname(os.getcwd()) + '\\' + TEST_RESULT_FILE_PATH
+        with open(self.nand_path, 'w') as nand_file:
             nand_file.write(initial_data)
-        ssd_driver = SSDDriverEnter(nand_path, result_path)
-        ssd_driver.read(0)
-        result = ""
-        with open(result_path, 'r') as result_file:
+        self.ssd_driver = SSDDriverEnter(self.nand_path, self.result_path)
+
+    def tearDown(self):
+        os.remove(self.nand_path)
+        os.remove(self.result_path)
+
+    def test_read_initialized_file(self):
+        self.ssd_driver.read(0)
+        result = ''
+        with open(self.result_path, 'r') as result_file:
             result += result_file.read()
-        os.remove(nand_path)
-        os.remove(result_path)
-        self.assertEqual("0x00000000", result)
+        self.assertEqual(INITIAL_VALUE, result)
+
