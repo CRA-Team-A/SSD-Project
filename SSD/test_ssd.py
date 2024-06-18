1e4508b2eec572215dba22d317edbb56bce9708a
Eunji.Heo <eunja.heo@samsung.com>
2024-06-18 14:41:38 +0900

    TEST: add Mock object for check call_count

--- SSD/test_ssd.py
+++ SSD/test_ssd.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
+from unittest.mock import Mock, patch
 
 from ssd_main import SSDApplication
-from ssd import SSDDriverComma
+from ssd import SSDDriverComma, SSDDriver
 import os
 
 
@@ -36,10 +37,16 @@ class TestSSDMain(TestCase):
         ret = app.main(["R", "100"])
         self.assertEqual(ret, False)
 
-    def test_main_read(self):
+    @patch.object(SSDApplication, "create_ssd_driver")
+    def test_main_read(self, mk_driver_factory):
+        mk = Mock(spec=SSDDriver)
+        mk.read.side_effect = "driver : read"
+        mk.write.side_effect = "driver : write"
+        mk_driver_factory.return_value = mk
         app = SSDApplication()
         ret = app.main(["R", "0"])
         self.assertEqual(ret, True)
+        self.assertEqual(mk.read.call_count, 1)
 
     def test_main_invalid_input_write(self):
         app = SSDApplication()
@@ -54,7 +61,13 @@ class TestSSDMain(TestCase):
         ret = app.main(["W", "0", "0x123456789"])
         self.assertEqual(ret, False)
 
-    def test_main_write(self):
+    @patch.object(SSDApplication, "create_ssd_driver")
+    def test_main_write(self, mk_driver_factory):
+        mk = Mock(spec=SSDDriver)
+        mk.read.side_effect = "driver : read"
+        mk.write.side_effect = "driver : write"
+        mk_driver_factory.return_value = mk
         app = SSDApplication()
         ret = app.main(["W", "0", "0x12345678"])
         self.assertEqual(ret, True)
+        self.assertEqual(mk.write.call_count, 1)
