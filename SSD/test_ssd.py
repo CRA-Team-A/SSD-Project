feaca3a0fef8ed371a58c8c2d553580370bea16b
Eunji.Heo <eunja.heo@samsung.com>
2024-06-18 14:50:34 +0900

    REFACTOR: extract setup method, add subTest in test_main_invalid_input_write

--- SSD/test_ssd.py
+++ SSD/test_ssd.py
@@ -30,44 +30,46 @@ class TestSSDDriver(TestCase):
 
 
 class TestSSDMain(TestCase):
+    def setUp(self):
+        self.app = SSDApplication()
+
     def test_main_invalid_input_read(self):
-        app = SSDApplication()
-        ret = app.main(["R", "-1"])
+        ret = self.app.main(["R", "-1"])
         self.assertEqual(ret, False)
-        ret = app.main(["R", "100"])
+        ret = self.app.main(["R", "100"])
         self.assertEqual(ret, False)
 
     @patch.object(SSDApplication, "create_ssd_driver")
     def test_main_read(self, mk_driver_factory):
-        mk = Mock(spec=SSDDriver)
-        mk.read.side_effect = "driver : read"
-        mk.write.side_effect = "driver : write"
+        mk = self.create_mock_ssd_driver()
         mk_driver_factory.return_value = mk
-        app = SSDApplication()
-        ret = app.main(["R", "0"])
+        ret = self.app.main(["R", "0"])
         self.assertEqual(ret, True)
         self.assertEqual(mk.read.call_count, 1)
 
     def test_main_invalid_input_write(self):
-        app = SSDApplication()
-        ret = app.main(["W", "-1", "0x00000000"])
-        self.assertEqual(ret, False)
-        ret = app.main(["W", "100", "0x00000000"])
-        self.assertEqual(ret, False)
-        ret = app.main(["W", "0", "1"])
-        self.assertEqual(ret, False)
-        ret = app.main(["W", "0", "0xFF"])
-        self.assertEqual(ret, False)
-        ret = app.main(["W", "0", "0x123456789"])
-        self.assertEqual(ret, False)
+        test_case = [
+            ["W", "-1", "0x00000000"],
+            ["W", "100", "0x00000000"],
+            ["W", "0", "1"],
+            ["W", "0", "0xFF"],
+            ["W", "0", "0x123456789"]
+        ]
+        for tc in test_case:
+            with self.subTest('sub_test arg : ' + " ".join(tc)):
+                ret = self.app.main(tc)
+                self.assertEqual(ret, False)
 
     @patch.object(SSDApplication, "create_ssd_driver")
     def test_main_write(self, mk_driver_factory):
+        mk = self.create_mock_ssd_driver()
+        mk_driver_factory.return_value = mk
+        ret = self.app.main(["W", "0", "0x12345678"])
+        self.assertEqual(ret, True)
+        self.assertEqual(mk.write.call_count, 1)
+
+    def create_mock_ssd_driver(self):
         mk = Mock(spec=SSDDriver)
         mk.read.side_effect = "driver : read"
         mk.write.side_effect = "driver : write"
-        mk_driver_factory.return_value = mk
-        app = SSDApplication()
-        ret = app.main(["W", "0", "0x12345678"])
-        self.assertEqual(ret, True)
-        self.assertEqual(mk.write.call_count, 1)
+        return mk
