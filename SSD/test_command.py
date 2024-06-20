a84b257f19851c37dad108606d31a622c5357128
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-20 11:48:03 +0900

    Test: Write Command 테스트 코드 추가

---
+++ SSD/test_command.py
@@ -0,0 +1,19 @@
+import os
+from unittest import TestCase
+
+from SSD.command import WriteCommand
+from SSD.ssd import SSDDriverCommon
+
+
+class TestCommand(TestCase):
+    def test_execute_write_success(self):
+        current_dir = None
+        if os.path.dirname(__file__) == '':
+            current_dir = os.getcwd()
+        else:
+            current_dir = os.path.dirname(__file__)
+        ssd_driver = SSDDriverCommon('\n', os.path.join(os.path.dirname(current_dir), 'nand.txt'),
+                                     os.path.join(os.path.dirname(current_dir), 'result.txt'))
+        write_command = WriteCommand(ssd_driver)
+        write_command.execute(1, value="0x00000001")
+        self.assertEqual(ssd_driver.read(1), 1)
