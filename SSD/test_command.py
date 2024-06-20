4733c4bd7979e5bee3d01fd25e39454cb150d6a3
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-20 14:15:09 +0900

    Refactor: Command Test 리팩토링 : 메서드 추출, 상수화

--- SSD/test_command.py
+++ SSD/test_command.py
@@ -4,34 +4,47 @@ from unittest import TestCase
 from SSD.command import WriteCommand
 from SSD.ssd import SSDDriverCommon
 
+if os.path.dirname(__file__) == '':
+    CURRENT_DIR = os.getcwd()
+else:
+    CURRENT_DIR = os.path.dirname(__file__)
+ROOT_DIR = os.path.dirname(CURRENT_DIR)
+NAND_PATH = os.path.join(ROOT_DIR, 'nand.txt')
+RESULT_PATH = os.path.join(ROOT_DIR, 'result.txt')
+
 
 class TestCommand(TestCase):
+    def setUp(self):
+        self.ssd_driver = SSDDriverCommon('\n', NAND_PATH, RESULT_PATH)
+        self.write_command = WriteCommand(self.ssd_driver)
+
+    @staticmethod
+    def clear_test_files(nand_path: str, result_path: str):
+        if os.path.exists(nand_path):
+            os.remove(nand_path)
+        if os.path.exists(result_path):
+            os.remove(result_path)
+        pass
+
+    def tearDown(self):
+        self.clear_test_files(NAND_PATH, RESULT_PATH)
+
+    def read_nand_file(self):
+        with open(NAND_PATH, "r") as f:
+            result_value = f.read().split(f"{self.ssd_driver.sep}")[1]
+        return result_value
+
+    @staticmethod
+    def read_result_file():
+        with open(RESULT_PATH, "r") as f:
+            result_value = f.read()
+        return result_value
 
     def test_execute_write_success(self):
-        current_dir = None
-        if os.path.dirname(__file__) == '':
-            current_dir = os.getcwd()
-        else:
-            current_dir = os.path.dirname(__file__)
-        ssd_driver = SSDDriverCommon('\n', os.path.join(os.path.dirname(current_dir), 'nand.txt'),
-                                     os.path.join(os.path.dirname(current_dir), 'result.txt'))
-        write_command = WriteCommand(ssd_driver)
-        write_command.execute(1, value="0x00000001")
-        with open(os.path.join(os.path.dirname(current_dir), 'nand.txt'), "r") as f:
-            result_value = f.read().split(f"{ssd_driver.sep}")[1]
-        self.assertEqual(int(result_value), 1)
+        self.write_command.execute(1, value="0x00000001")
+        self.assertEqual(int(self.read_nand_file()), 1)
 
     def test_execute_write_read_success(self):
-        current_dir = None
-        if os.path.dirname(__file__) == '':
-            current_dir = os.getcwd()
-        else:
-            current_dir = os.path.dirname(__file__)
-        ssd_driver = SSDDriverCommon('\n', os.path.join(os.path.dirname(current_dir), 'nand.txt'),
-                                     os.path.join(os.path.dirname(current_dir), 'result.txt'))
-        write_command = WriteCommand(ssd_driver)
-        write_command.execute(1, value="0x00000001")
-        ssd_driver.read(1)
-        with open(os.path.join(os.path.dirname(current_dir), 'result.txt'), "r") as f:
-            result_value = f.read()
-        self.assertEqual(result_value, "0x00000001")
+        self.write_command.execute(1, value="0x00000001")
+        self.ssd_driver.read(1)
+        self.assertEqual(self.read_result_file(), "0x00000001")
