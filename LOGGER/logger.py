14b73c715fd83e5d7b89048c7a8e5a8b4ae21d23
shpark <sh0913.park@samsung.com>
2024-06-20 11:27:42 +0900

    feat: develop Logger

--- LOGGER/logger.py
+++ LOGGER/logger.py
@@ -12,6 +12,7 @@ else:
 ROOT_DIR = os.path.dirname(CURRENT_DIR)
 LOG_EXT = ".log"
 ZIP_EXT = ".zip"
+PREFIX = "until_"
 
 
 class Logger(SingletonClass):
@@ -22,22 +23,33 @@ class Logger(SingletonClass):
 
     def log(self, func_name, message):
         func = func_name + "()"
-        text = f"[{self.get_now()}] {func:<30}: {message}\n"
+        self.save_latest(f"[{self.get_now()}] {func:<30}: {message}\n")
+        if self.is_oversized_latest():
+            self.save_oversized_log()
+            self.covert_log_to_zip()
+
+    def covert_log_to_zip(self):
+        previous_file_list = self.get_log_list()
+        for file in previous_file_list[:-1]:
+            file.rename(str(file).replace(LOG_EXT, ZIP_EXT))
+
+    def save_oversized_log(self):
+        log_name = join(self.root, PREFIX + self.get_now() + LOG_EXT)
+        self.latest.rename(log_name)
+        self.latest.touch(exist_ok=True)
+
+    def is_oversized_latest(self):
+        return self.latest.stat().st_size > self.threshold
+
+    def save_latest(self, log_text):
         with open(self.latest, 'a') as log_file:
-            log_file.write(text)
-        if self.latest.stat().st_size > self.threshold:
-            log_name = join(self.root, f"until_{self.get_now()}.log")
-            self.latest.rename(log_name)
-            self.latest.touch(exist_ok=True)
-            previous_file_list = sorted([Path(join(self.root, file)) for file in os.listdir(self.root) if file.endswith('.log') and file.startswith('until_')],
-                               key=lambda f: os.path.getctime(os.path.join(self.root, f)), reverse=True)
-            for file in previous_file_list[:-1]:
-                file.rename(str(file).replace('.log', '.zip'))
+            log_file.write(log_text)
+
+    def get_log_list(self):
+        log_list = [Path(join(self.root, file)) for file in os.listdir(self.root)
+                    if file.endswith(LOG_EXT) and file.startswith(PREFIX)]
+        return sorted(log_list, key=lambda f: os.path.getctime(os.path.join(self.root, f)), reverse=True)
 
     @staticmethod
     def get_now():
         return datetime.now().strftime('%y-%m-%d_%H-%M-%S')
-
-
-
-
