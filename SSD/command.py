c1aace0929def15a64ef3e8e11b35fa57230537b
jwoong23.seo <jwoong23.seo@samsung.com>
2024-06-20 15:07:05 +0900

    Refactor: Logger 클래스 수정 및 command logger 추가

--- SSD/command.py
+++ SSD/command.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+from LOGGER.logger import Logger
 from SSD.ssd import SSDDriver
 
 
 class Command(ABC):
     def __init__(self, driver: SSDDriver):
         self.ssd_driver = driver
+        self.logger = Logger()
 
     @abstractmethod
     def execute(self, address: int, value: str):
@@ -14,9 +16,17 @@ class Command(ABC):
 
 class WriteCommand(Command):
     def execute(self, address: int, value: str):
-        self.ssd_driver.write(address, value)
+        try:
+            self.ssd_driver.write(address, value)
+            self.logger.log(f'{self.__class__.__name__}.{self.execute.__name__} success!')
+        except Exception:
+            self.logger.log(f'{self.__class__.__name__}.{self.execute.__name__} fail! Error 내용 : {str(Exception)}')
 
 
 class EraseCommand(Command):
     def execute(self, address: int, value: str = None):
-        self.ssd_driver.erase(address)
+        try:
+            self.ssd_driver.erase(address)
+            self.logger.log(f'{self.__class__.__name__} 클래스 {self.execute.__name__} success!')
+        except Exception:
+            self.logger.log(f'{self.__class__.__name__} 클래스 {self.execute.__name__} fail! Error 내용 : {str(Exception)}')
