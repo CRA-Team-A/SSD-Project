026c8b407cb52da0a28a511d5a79520ed0043386
gardenk <jwnnekim@gmail.com>
2024-06-20 11:16:14 +0900

    FEAT: initial erase & erase_range

--- SHELL/TestShellApplication.py
+++ SHELL/TestShellApplication.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+ERASE_RANGE_CODE = 'erase_range'
+
 MAX_ADDRESS_FOR_FULL = 100
 
 EXIT_CODE = 'exit'
@@ -9,6 +11,7 @@ FULLWRITE_CODE = 'fullwrite'
 READ_CODE = 'read'
 FULLREAD_CODE = 'fullread'
 HELP_CODE = 'help'
+ERASE_CODE = 'erase'
 
 TESTAPP2 = 'testapp2'
 TESTAPP1 = 'testapp1'
@@ -36,6 +39,7 @@ class TestShellApplication:
         self.execution = None
         self.address = None
         self.data = None
+        self.size = None
 
     def go_execution(self):
         if self.execution == WRITE_CODE:
@@ -52,6 +56,10 @@ class TestShellApplication:
             return self.test_app_1()
         elif self.execution == TESTAPP2:
             return self.test_app_2()
+        elif self.execution == ERASE_CODE:
+            return self.erase()
+        elif self.execution == ERASE_RANGE_CODE:
+            return self.erase_range()
 
     def test_app_1(self):
         write_data = '0xABCDFFFF'
@@ -80,6 +88,12 @@ class TestShellApplication:
             self.data = command[1]
         elif self.execution == READ_CODE:
             self.address = command[1]
+        elif self.execution == ERASE_CODE:
+            self.address = command[1]
+            self.size = command[2]
+        elif self.execution == ERASE_RANGE_CODE:
+            self.address = command[1]
+            self.size = str(int(command[2]) - int(command[1]))
 
         return True
 
@@ -91,6 +105,8 @@ class TestShellApplication:
             params = [self.execution, self.address, self.data]
         elif self.execution == 'R':
             params = [self.execution, self.address]
+        elif self.execution == 'E':
+            params = [self.execution, self.address, self.size]
         else:
             raise NotImplementedError
         result = subprocess.run(['python', SSD_PATH] + params, capture_output=True, text=True, check=True)
@@ -140,6 +156,23 @@ class TestShellApplication:
             'To repeat this information : help',
             sep='\n')
 
+    def erase(self):
+        self.execution = 'E'
+        return self.run_subprocess()
+
+    def erase_range(self):
+        if int(self.size) > 10:
+            total_size = int(self.size)
+            div = 10
+            while total_size:
+                self.size = str(min(div, total_size))
+                result = self.erase()
+                if result == False:
+                    return False
+                total_size = max(0, total_size - div)
+        else:
+            self.erase()
+
     def is_valid_address(self, address: int):
         for num in address:
             if not ord('0') <= ord(num) <= ord('9'):
@@ -156,6 +189,14 @@ class TestShellApplication:
         for num in input_data[2:]:
             if not (ord('0') <= ord(num) <= ord('9') or ord('A') <= ord(num) <= ord('F')):
                 return False
+        return True
+
+    def is_valid_size(self, size: str):
+        for num in size:
+            if not ord('0') <= ord(num) <= ord('9'):
+                return False
+        if int(size) <= 0 or int(size) > 10:
+            return False
         return True
 
     def is_valid_command(self, input_command_elements: list):
@@ -202,6 +243,21 @@ class TestShellApplication:
             if len(input_command_elements) != 1:
                 return False
             return True
+        elif input_command_elements[0] == ERASE_CODE:
+            if len(input_command_elements) != 3:
+                return False
+            if not self.is_valid_address(input_command_elements[1]):
+                return False
+            if not self.is_valid_size(input_command_elements[2]):
+                return False
+            return True
+        elif input_command_elements[0] == ERASE_RANGE_CODE:
+            if len(input_command_elements) != 3:
+                return False
+            if not self.is_valid_address(input_command_elements[1]):
+                return False
+            if not self.is_valid_address(input_command_elements[2]):
+                return False
         return False
 
     def test_app_2(self):
