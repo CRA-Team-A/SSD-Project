e8b7fae36d25b053f962bfff3a35a8474f0414a6
gardenk <jwnnekim@gmail.com>
2024-06-24 10:53:36 +0900

    FEAT: add log for ssd_handler

--- SHELL/ssd_handler.py
+++ SHELL/ssd_handler.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+from LOGGER.logger import Logger
 
 SSD_PATH = 'SSD/ssd_interface.py'
 
@@ -13,15 +14,28 @@ class SSDHandler:
             current_dir = os.path.dirname(__file__)
         root_dir = os.path.dirname(current_dir)
         self.path = os.path.join(root_dir, SSD_PATH)
+        self.logger = Logger()
 
     def read(self, *args):
-        ret = subprocess.run(["python", self.path, "R"] + [str(i) for i in args], capture_output=True, text=True, check=True)
-        return ret.returncode
+        try:
+            ret = subprocess.run(["python", self.path, "R"] + [str(i) for i in args], capture_output=True, text=True, check=True)
+            return ret.returncode
+        except:
+            self.logger.log(message=f'FAILED subprocess <R {[str(i) for i in args]}>')
+            return -1
 
     def write(self, *args):
-        ret = subprocess.run(["python", self.path, "W"] + [str(i) for i in args], capture_output=True, text=True, check=True)
-        return ret.returncode
+        try:
+            ret = subprocess.run(["python", self.path, "W"] + [str(i) for i in args], capture_output=True, text=True, check=True)
+            return ret.returncode
+        except:
+            self.logger.log(message=f'FAILED subprocess <W {[str(i) for i in args]}>')
+            return -1
 
     def erase(self, *args):
-        ret = subprocess.run(["python", self.path, "E"] + [str(i) for i in args], capture_output=True, text=True, check=True)
-        return ret.returncode
+        try:
+            ret = subprocess.run(["python", self.path, "E"] + [str(i) for i in args], capture_output=True, text=True, check=True)
+            return ret.returncode
+        except:
+            self.logger.log(message=f'FAILED subprocess <E {[str(i) for i in args]}>')
+            return -1
